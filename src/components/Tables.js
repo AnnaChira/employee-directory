bbd7b87dfa80fdde45998178336b57647b35bdf7
Anna Chirapongse <achirapongse@gmail.com>
2021-01-22 21:13:57 -0500

    Updates to Readme

--- src/components/Tables.js
+++ src/components/Tables.js
@@ -1,37 +1,19 @@
 import React from "react";
-import EmployeeCard from "./components/EmployeeCard";
-import Wrapper from "./components/Wrapper";
+import EmployeeCard from "../components/EmployeeCard";
+import Wrapper from "../components/Wrapper";
 import "./style.css";
-// import API from "../services/api.js";
 import employees from "/employees.json";
 
 class Table extends Component {
-  // Setting this.state.friends to the friends json array
   state = {
     employees
   };
 
   removeEmployee = id => {
-    // Filter this.state.friends for friends with an id not equal to the id being removed
     const employees = this.state.employees.filter(employees => employees.id !== id);
-    // Set this.state.friends equal to the new friends array
     this.setState({ employees });
   };
 
-// class Table extends React.Component {
-//     constructor() {
-//         super()
-//         this.getUsers()
-//     }
-    
-//     // Setting the initial state of the Counter component
-//     state = {
-//       users:[]
-//     };
-  
-//     // handleIncrement increments this.state.count by 1
-//     handleIncrement = () => {
-      // We always use the setState method to update a component's state
       render() {
         return (
           <Wrapper>
@@ -52,14 +34,5 @@ class Table extends Component {
       }  
     
     };
-    // getUsers() {
-    //     return API.getUsers()
-    //     .then(function(results){
-    //         console.log(results)
-    //         this.setState({ users: results.data});
-    //     })
-    // }
-    // The render method returns the JSX that should be rendered
-
 
 export default Table;
